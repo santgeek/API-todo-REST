2c2653bbcf136db258d10bb737b3a289ec1a8442
Santiago Montoya <santiago.mordz@gmail.com>
2024-09-28 19:47:22 +0000

    feat(API): save todos at 4Geeks server

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	let [inputValue, setInputValue] = useState("");
+	let [arrToDo, setArrToDo] = useState([])
+	let [hoverIndex, setHoverIndex] = useState(null)
+
+	const handleKeyDown = (e) => {
+		if (inputValue.trim() === "") return
+		if (e.key === "Enter") {
+			setArrToDo([...arrToDo, inputValue.trim()]);
+			setInputValue("");
+		}
+	};
+
+	const handleDelete = (index) => {
+		setArrToDo(arrToDo.filter((curr, i) => i !== index));
+	};
+
+	useEffect(() => {
+		const getUserInfo = async () => {
+			const response = await fetch('https://playground.4geeks.com/todo/users/santiago');
+			const data = await response.json();
+			console.log(data)
+		}
+		getUserInfo();
+	}, [])
+
+	//addNewToDo
+	const newItem = { label: inputValue };
+	useEffect(() => {
+		if (inputValue) {
+			fetch('https://playground.4geeks.com/todo/todos/santiago', {
+				method: "POST",
+				body: JSON.stringify(newItem),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => resp.json())
+				.then(data => {
+					console.log(data)
+				})
+				.catch(error => {
+					console.log("Error", error)
+				})
+		}
+	}, [inputValue])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-50 text-center mt-5 d-flex flex-column">
+			<div className="mb-2">
+				<h1 className="display-2 fw-light text-muted">todos</h1>
+			</div>
+
+			<ul className="list-group rounded-0">
+				<li className="list-group-item input-group-md">
+					<input
+						className="border-0 d-block text-start form-control text-secondary fs-4"
+						type="text"
+						onKeyDown={handleKeyDown}
+						onChange={e => setInputValue(e.target.value)}
+						value={inputValue}
+						placeholder={arrToDo.length === 0 ? "No hay tareas, añadir tareas" : ""}
+					/>
+				</li>
+				{arrToDo.map((element, index) =>
+					<li
+						key={index}
+						className="list-group-item d-flex justify-content-between text-secondary fs-4"
+						onMouseEnter={() => setHoverIndex(index)}
+						onMouseLeave={() => setHoverIndex(null)}
+					> {element}
+						{hoverIndex === index && (
+							<button
+								className="btn btn-sm justify-content-between"
+								onClick={() => handleDelete(index)}
+							>
+								x
+							</button>
+						)}
+					</li>
+
+				)}
+				<li className="list-group-item text-start text-secondary">
+					<h6 className="h6 fw-lighter"><small>{arrToDo.length} items left</small> </h6>
+				</li>
+				<div className="backgroundBox1"></div>
+				<div className="backgroundBox2"></div>
+			</ul>
+
+			<div className="background-box">back1</div>
+			<div className="background-box2">back2</div>
+			<button id="clean" type="button" className="btn btn-lg">Limpiar lista</button>
 		</div>
 	);
 };
